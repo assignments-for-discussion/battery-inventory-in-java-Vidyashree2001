b8f88793837cd0bd855eea2d71af7702abccc19f
Vidyashree2001 <103561293+Vidyashree2001@users.noreply.github.com>
2022-09-22 18:13:18 +0530

    Update Main.java

--- bunchbycycles/Main.java
+++ bunchbycycles/Main.java
@@ -14,6 +14,10 @@ public class Main {
 
   static void testBucketingByNumberOfCycles() {
     System.out.println("Counting batteries by usage cycles...\n");
+    ArrayList<Integer> high=new ArrayList<> ();
+     ArrayList<Integer> low=new ArrayList<> ();
+    ArrayList<Integer> medium=new ArrayList<> ();
+    
     CountsByUsage counts = countBatteriesByUsage(new int[] {100, 300, 500, 600, 900, 1000});
     assert(counts.lowCount == 2);
     assert(counts.mediumCount == 3);
@@ -22,11 +26,38 @@ public class Main {
       
     {
       if(counts[i]>910)
+      {
+        high.add(counts[i]);
       highCount++;
-    else  if(counts[i]>=410 || counts[i]<=909)
-      mediuCount++;
-    else 
-      lowCount++;
+      }
+     
+   
+      else  if(counts[i]>=410 && counts[i]<=909)
+      {
+         medium.add(counts[i]);
+        mediuCount++;
+      }
+    
+      else 
+      {
+         low.add(counts[i]);
+        lowCount++;
+      }
+      
+    }
+    for(int arr1:high)
+    {
+       System.out.println("The battery charged more than 910 is:" +arr1+ "and count is" +highCount);
+    }
+     for(int arr2:medium)
+    {
+         System.out.println("The battery charged between  410 and  910 is:" +arr2+ "and count is" +midCount);
+    } 
+    for(int arr3:low)
+    {
+      System.out.println("The battery charged less than 410 is:" +arr3+ "and count is" +lowCount);
+    }
+    
     System.out.println("Done counting :)\n");
   }
 
